c97c1ac8718c82c45bcd5383f67a0118c5e6fced
TianaSaurusWrecks <tianavioletwilson@gmail.com>
2025-10-31 11:28:06 +1100

    Question 3.1
    Defined Strings

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/*implement a range of algorithms to manipulate strings-->
+
+ Question 3.1 Define the strings */
+let myString = "This is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Tiana";
+
+console.log("Strings:");
+console.log(myString);
+console.log(anotherString); 
+console.log(hello);
+console.log(myName);
